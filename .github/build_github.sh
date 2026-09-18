276b3426f5b1fd99c11ee486a1266224c45b280b
Mod4 <omarelfarok24135@gmail.com>
2026-09-18 16:29:19 +0400

    Update build_github.sh

--- .github/build_github.sh
+++ .github/build_github.sh
@@ -41,7 +41,7 @@ mv ./tmp/SideStoreSupport.framework Payload/LiveContainer.app/Frameworks
 
 # download SideStore
 cd tmp
-wget https://github.com/SideStore/SideStore/releases/download/nightly/SideStore.ipa
+wget https://github.com/SideStore/SideStore/releases/download/0.7.0-alpha/SideStore.ipa
 unzip SideStore.ipa
 cd ..
 
